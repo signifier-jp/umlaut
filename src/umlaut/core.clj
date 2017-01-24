2cca6bc2bcf60d836238665a1a0982557c1c436e
Tiago Luchini <luchini@work.co>
2017-01-23 23:22:43 -0500

    Transformers added

--- src/umlaut/core.clj
+++ src/umlaut/core.clj
@@ -8,29 +8,60 @@
   (insta/parser
     (io/resource "umlaut.bnf")))
 
+(defn- normalize-arity
+  [args]
+  (case (count args)
+    0 [1 1]
+    1 (let [lone (nth args 0)]
+        (if (= "n" lone) [1 lone] [lone lone]))
+    2 (vec args)))
+
 (defn- to-enum
-  [id & args] {:enum {:id id}
-               :values args})
+  [id & args] {:enum {:id id
+               :values args}})
 
-(defn- to-optional
-  [& args] {:optional true})
-
-(defn- to-single
+(defn- to-kind
   [id & args] {:type id
-               :arity [1]})
+               :arity (normalize-arity args)})
 
-(defn- to-collection
-  [id & args] {:type id
-               :arity (vec args)})
+(defn- to-attribute
+  [id & args] (assoc (first args) :id id :relationship-type :attribute))
 
-(def transformers
-  {:enum to-enum
-   :from read-string
-   :to #(if (not=  "n" %) (read-string %) %)
-   :single to-single
-   :collection to-collection
-   :optional to-optional})
+(defn- to-parent
+  [type] {:type type :relationship-type :parent})
 
+(defn- filter-relationsip-type
+  [relationship-id coll]
+  (vec (map
+    #(dissoc % :relationship-type)
+    (filter
+      #(= relationship-id (:relationship-type %))
+      coll))))
+
+(defn- abstract-to-type
+  [type] (fn
+           [id & args]
+           {type {:id id
+                  :attributes (filter-relationsip-type :attribute args)
+                  :parents (filter-relationsip-type :parent args)}}))
+
+(defn- to-diagram
+  [id & args] {:diagram {:id id
+                         :groups (vec args)}})
+
+(defn- to-diagram-group
+  [& args] (vec args))
+
+(defn- transformer
+  [args] (insta/transform {:enum to-enum
+                             :arity-value #(if (not= "n" %) (read-string %) %)
+                             :kind to-kind
+                             :type (abstract-to-type :type)
+                             :parent to-parent
+                             :interface (abstract-to-type :interface)
+                             :attribute to-attribute
+                             :diagram to-diagram
+                             :diagram-group to-diagram-group} args))
 
 (defn -main
   [path & args]
@@ -39,11 +70,12 @@
        (file-seq)
        (map #(.getPath %))
        (filter #(str/ends-with? % ".umlaut"))
-       (map #(slurp %))
-       (reduce #(conj %1 (parse %2)) '())))
+       (map (fn [x] {:file-name x :content (slurp x)}))
+       (reduce
+         #(assoc %1 (:file-name %2)
+                    (->> (parse (:content %2)) transformer))
+         '{})))
 
-;(-main "test/sample")
+(-main "test/sample")
 
-(parse (slurp "test/sample/sample.umlaut"))
-
-(insta/transform transformers (parse (slurp "test/sample/sample.umlaut")))
+;(parse (slurp "test/sample/sample.umlaut"))
