58fc093f875cdb93bf7af443faccb1585ea4140d
Nikolas Moya <moya@work.co>
2017-05-01 12:48:16 +0200

    Fix tests

--- test/umlaut/all_test.clj
+++ test/umlaut/all_test.clj
@@ -3,7 +3,8 @@
             [umlaut.core-parser-test :as core]
             [umlaut.dot-test :as dot]
             [umlaut.utils-test :as utils]
-            [umlaut.lacinia-test :as lacinia]))
+            [umlaut.lacinia-test :as lacinia]
+            [umlaut.graphql-test :as graphql]))
 
 (run-tests (find-ns 'umlaut.core-parser-test)
            (find-ns 'umlaut.dot-test)
