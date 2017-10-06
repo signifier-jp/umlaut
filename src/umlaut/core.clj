fdf2ac72a642733a56dfa2352428dc24a9f26095
Nikolas Moya <moya@work.co>
2017-10-06 17:43:23 +0200

    Add type checking before returning the structure to the generators
    Also updated tests and fixtures to fix error in own umlaut test file.

--- src/umlaut/core.clj
+++ src/umlaut/core.clj
@@ -19,13 +19,57 @@
                    (utils/map-extend acc {:nodes (parsed :nodes)
                                           :diagrams (parsed :diagrams)}))) {})))
 
+(defn- get-all-fields
+  [parsed]
+  (map (fn [[key [kind type-obj]]]
+          :fields (:fields type-obj))
+    (seq (:nodes parsed))))
+
+(defn- check-field-type
+  [valid-types field]
+  (let [field-type (:type-id (:return field))]
+    (if (not (utils/in? field-type valid-types))
+      (str "Field with invalid type: '" (:id field) "' cannot be '" field-type "'")
+      nil)))
+
+(defn- validate-types
+  [parsed]
+  (let [declared-types (keys (:nodes parsed))
+        valid-types (concat declared-types utils/primitive-types)
+        all-fields (get-all-fields parsed)]
+    (remove nil?
+      (reduce
+        (fn [acc fields]
+          (concat acc (map (partial check-field-type valid-types) fields)))
+        []
+        all-fields))))
+
+(defn- check-throw-spec-error
+  [parsed]
+  (if (not (s/valid? ::model/namespaces parsed))
+    (throw (Exception. (s/explain ::model/namespaces parsed)))
+    parsed))
+
+(defn- format-type-errors
+  [errors]
+  (reduce
+    (fn [acc error]
+      (str acc error "\n"))
+    "\n"
+    errors))
+
+(defn- check-throw-type-error
+  [parsed]
+  (let [result (validate-types parsed)]
+    (if (> (count result) 0)
+      (throw (Exception. (format-type-errors result)))
+      parsed)))
+
 (defn- read-folder [path]
   "Validate all the umlaut code parsed from a folder"
-  read-parse path
-  (let [parsed (read-parse path)]
-    (if (not (s/valid? ::model/namespaces parsed))
-      (throw (Exception. (s/explain ::model/namespaces parsed)))
-      parsed)))
+  (-> (read-parse path)
+    check-throw-spec-error
+    check-throw-type-error))
 
 (defn main
   "Parses, validates, and transform the umlaut files from a folder"
