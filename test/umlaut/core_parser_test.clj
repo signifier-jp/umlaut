150e13939bab7fd9eb320431a9cad8e1b24d00cb
Nikolas Moya <moya@work.co>
2017-10-06 15:36:59 +0200

    Add more special character in the test files
    No actions were required, the grammar was already supporting a vast
    range of special characters inside the @doc string.

--- test/umlaut/core_parser_test.clj
+++ test/umlaut/core_parser_test.clj
@@ -1,13 +1,18 @@
 (ns umlaut.core-parser-test
   (:require [clojure.test :refer :all]
             [umlaut.core :refer :all]
-            [umlaut.parser :refer :all]))
+            [umlaut.parser :refer :all]
+            [clojure.data :as data]))
 (use '[clojure.pprint :only [pprint]])
 
 (def fixture (read-string (slurp "test/fixtures/person/umlaut.fixture")))
 
 (deftest core-test
-  (testing "Umlaut input"
-    (is (= fixture (umlaut.core/main ["test/fixtures/person/person.umlaut" "test/fixtures/person/profession.umlaut"])))))
+  (testing "Umlaut input")
+  (let [diff (data/diff fixture
+              (umlaut.core/main ["test/fixtures/person/person.umlaut" "test/fixtures/person/profession.umlaut"]))]
+    (is (and
+          (nil? (first diff))
+          (nil? (second diff))))))
 
 (run-all-tests)
--- test/umlaut/lacinia_test.clj
+++ test/umlaut/lacinia_test.clj
@@ -1,13 +1,18 @@
 (ns umlaut.lacinia-test
   (:require [clojure.test :refer :all]
             [umlaut.core :as core]
-            [umlaut.generators.lacinia :as lacinia]))
+            [umlaut.generators.lacinia :as lacinia]
+            [clojure.data :as data]))
 (use '[clojure.pprint :only [pprint]])
 
 (def fixture (read-string (slurp "test/fixtures/person/lacinia.fixture")))
 
 (deftest lacinia-test
   (testing "Lacinia generator test"
-    (is (= fixture (lacinia/gen ["test/fixtures/person/person.umlaut" "test/fixtures/person/profession.umlaut"])))))
+    (let [diff (data/diff fixture
+                (lacinia/gen ["test/fixtures/person/person.umlaut" "test/fixtures/person/profession.umlaut"]))]
+      (is (and
+            (nil? (first diff))
+            (nil? (second diff)))))))
 
 (run-all-tests)
